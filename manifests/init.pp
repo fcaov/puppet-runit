fe8b961d22efa7acff28c8e5c71010f140342eab
Fei Cao <fcao@vmware.com>
2014-05-12 10:32:25 -0700

    contiditionalize the package install

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,11 @@
 class runit( $ensure = present ) {
 
-  package { runit: ensure => $ensure }
+  #This following case condition is due to that we have not found rpm repo containing runit rpm
+  case $::osfamily {
+    'Debian': {
+      package { runit: ensure => $ensure }
+    }
+  }
 
   if $ensure == present {
     file {
